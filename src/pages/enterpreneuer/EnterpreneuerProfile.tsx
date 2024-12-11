16be7be54f9baf6276f318c31cfc6de100bae197
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-12-11 23:38:03 +0400

    inputs

--- src/pages/enterpreneuer/EnterpreneuerProfile.tsx
+++ src/pages/enterpreneuer/EnterpreneuerProfile.tsx
@@ -4,7 +4,7 @@ export default function EnterpreneuerProfile() {
   return (
     <div className="bg-[#C7D9FF] flex  flex-col items-center px-[9px] py-[77px] gap-[77px]">
       <h1 className="text-[#152C5E] text-[24px] font-semibold">ჩემი პროფილი</h1>
-      <div>
+      {/* <div>
         <input
           type="file"
           id="image"
@@ -12,18 +12,60 @@ export default function EnterpreneuerProfile() {
           accept=".jpg, .jpeg, .png, .gif"
           className=" border border-[dashed] rounded-full h-[50px]"
         />
-      </div>
-      <div className="relative">
-        <input type="file" id="file-input" className="hidden" />
+      </div> */}
+      <div className="flex flex-col items-center gap-[39px] w-full">
+        <div className="relative">
+          <input type="file" id="file-input" className="hidden" />
 
-        <label
-          for="file-input"
-          class="cursor-pointer bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600"
-        >
-          Upload File
-        </label>
+          <label
+            htmlFor="file-input"
+            className="cursor-pointer bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600"
+          >
+            Upload File
+          </label>
 
-        <span id="file-name" className="ml-4 text-gray-600"></span>
+          <span id="file-name" className="ml-4 text-gray-600"></span>
+        </div>
+        <div className="inputsDivsInProfile">
+          <span>&#8226; </span>
+          <input
+            type="text"
+            className="inputsInProfile"
+            placeholder="zuraaaaaaaaaaaaaaaa"
+          />
+        </div>
+        <div className="inputsDivsInProfile">
+          <span>&#8226; </span>
+          <input type="text" className="inputsInProfile" />
+        </div>
+        <div>
+          <span>&#8226; </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>&#8226; </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>&#8226; </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>&#8226; </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>&#8226; </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>&#8226; </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>&#8226; </span>
+          <input type="text" />
+        </div>
       </div>
     </div>
   );
